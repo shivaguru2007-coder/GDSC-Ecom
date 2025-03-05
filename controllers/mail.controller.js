cf25b5cefc0e877ba070c882c783c001892c7475
Shivaguru P <lakshmikodi1982@gmail.com>
2025-03-06 00:56:38 +0530

    fix: visible credentials changed

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -4,16 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "afkfg04@gmail.com",
-      pass: "etjo hdsh hqhs cpqb",
-
-    },
-  });
+  service: process.env.SMTP_USERNAME,
+  host: process.env.SMTP_SERVER,
+  port: process.env.SMTP_PORT,
+  secure: true,
+  auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD
+  },
+});
   
 async function mailSender(to, subject, content){
     transporter.sendMail({
